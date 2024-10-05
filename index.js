6e4354a9149d123b3bed58fff9e2753c8707f278
unknown <mosusmoss@gmail.com>
2024-10-05 20:57:48 +0300

    update

--- index.js
+++ index.js
@@ -3,23 +3,22 @@
  ---------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+  if (e.key === "Tab") {
+    document.body.classList.add("user-is-tabbing");
 
-    self.removeEventListener('keydown', handleFirstTab)
-    self.addEventListener('mousedown', handleMouseDownOnce)
+    self.removeEventListener("keydown", handleFirstTab);
+    self.addEventListener("mousedown", handleMouseDownOnce);
   }
-
-}
+};
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+  document.body.classList.remove("user-is-tabbing");
 
-  self.removeEventListener('mousedown', handleMouseDownOnce)
-  self.addEventListener('keydown', handleFirstTab)
-}
+  self.removeEventListener("mousedown", handleMouseDownOnce);
+  self.addEventListener("keydown", handleFirstTab);
+};
 
-self.addEventListener('keydown', handleFirstTab)
+self.addEventListener("keydown", handleFirstTab);
 
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
