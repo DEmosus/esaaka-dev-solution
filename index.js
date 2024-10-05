8ec124f98158253cbad7511b59e55db1d60bd6c8
unknown <mosusmoss@gmail.com>
2024-10-05 19:20:55 +0300

    nasa-project

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const handleFirstTab = (e) => {
   if(e.key === 'Tab') {
     document.body.classList.add('user-is-tabbing')
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
+    self.removeEventListener('keydown', handleFirstTab)
+    self.addEventListener('mousedown', handleMouseDownOnce)
   }
 
 }
@@ -15,11 +15,11 @@ const handleFirstTab = (e) => {
 const handleMouseDownOnce = () => {
   document.body.classList.remove('user-is-tabbing')
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
+  self.removeEventListener('mousedown', handleMouseDownOnce)
+  self.addEventListener('keydown', handleFirstTab)
 }
 
-window.addEventListener('keydown', handleFirstTab)
+self.addEventListener('keydown', handleFirstTab)
 
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
@@ -32,8 +32,8 @@ let alterStyles = (isBackToTopRendered) => {
     : "scale(0)";
 };
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
+self.addEventListener("scroll", () => {
+  if (self.scrollY > 700) {
     isBackToTopRendered = true;
     alterStyles(isBackToTopRendered);
   } else {
